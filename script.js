82138f343d9bfb7cb62ba586a577a0791086fb24
Dejmo <122269801+Dejmo@users.noreply.github.com>
2023-01-26 15:41:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+	let firstNumber = prompt('What is your 1st number');
+	let secondNumber = prompt('What is your 2nd number');
+	let question = prompt('What do u want to do?\nAdd, Minus, Divide or Multiply?');
+	let sum = question.toUpperCase();
+
+		if (sum == 'ADD') {
+				let sumAdd = Number(firstNumber) + Number(secondNumber);
+				 console.log(`${firstNumber} + ${secondNumber} =` + ' ' + sumAdd);
+		} else if (sum == 'MINUS') {
+				let sumMinus = Number(firstNumber) - Number(secondNumber);
+				 console.log(`${firstNumber} - ${secondNumber} =` + ' ' + sumMinus);
+		} else if (sum == 'DIVIDE') {
+				let sumDivide = Number(firstNumber) / Number(secondNumber);
+				 console.log(`${firstNumber} / ${secondNumber} =` + ' ' + sumDivide);
+		} else if (sum == 'MULTIPLY') {
+				let sumMultiply = Number(firstNumber) * Number(secondNumber);
+				 console.log(`${firstNumber} * ${secondNumber} =` + ' ' + sumMultiply);
+		} else {
+				 console.log('adadada')
+		}
